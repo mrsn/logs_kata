55f79a29d69c872bebc74e1a5d072ac8111e26ab
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2015-12-15 10:20:21 +0100

    refactoring

--- logs_extractor.rb
+++ logs_extractor.rb
@@ -3,7 +3,7 @@ def extract_message_id_and_subject_from(log_content)
 end
 
 def print(message_ids_with_subjects)
-  ids_with_subject.each do |pair|
+  message_ids_with_subjects.each do |pair|
     puts "message_id: #{pair.first} with subject #{pair.last}"
   end
 end
@@ -14,4 +14,3 @@ log_content = file.read
 file.close
 
 print(extract_message_id_and_subject_from(log_content))
-
