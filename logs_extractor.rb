148ddd01db11ee509f6d4702b1b90c92b1dfc29f
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2015-12-15 10:12:56 +0100

    rename

---
+++ logs_extractor.rb
@@ -0,0 +1,17 @@
+def extract_message_id_and_subject_from(log_content)
+  log_content.scan(/id=(.+@.+)T=("[^"]+")/)
+end
+
+def print(message_ids_with_subjects)
+  ids_with_subject.each do |pair|
+    puts "message_id: #{pair.first} with subject #{pair.last}"
+  end
+end
+
+file = File.open('test-sample.log', 'r')
+
+log_content = file.read
+file.close
+
+print(extract_message_id_and_subject_from(log_content))
+
